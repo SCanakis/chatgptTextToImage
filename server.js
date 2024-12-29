5d0c8a4606ec1b929bcd8349d5af6373a25bcf90
Sebastian Canakis <scanakis@SebastiansMBP3.lan>
2024-12-29 18:20:54 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI();
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/dream', async (req,res) => {
+    const prompt = req.body.prompt;
+
+    const aiResponse = await openai.images.generate({
+        model: "dall-e-2",
+        prompt: prompt,
+        n:1,
+        size: '1024x1024'
+    });
+
+    const image = aiResponse.data[0].url;
+    res.send({image});
+});
+
+app.listen(8080, () => console.log("make art on http://localhost:8080/dream"));
